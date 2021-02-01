02228a1b4f8c7072fa3f471cedd5cb4eb4f59b8a
inffinite <louislaizer4@gmail.com>
2021-02-01 10:14:43 +0300

    First commit: copy files module operational

---
+++ bin/index.js
@@ -0,0 +1,54 @@
+const argv = require('yargs')
+    .option('filter', {
+        alias: 'f',
+        describe: 'Filter for specific file or files.\n'
+    })
+    .option('path', {
+        alias: 'p',
+        describe: 'Path to files.\n Defaults to current directory\n'
+    })
+    .option('dest', {
+        alias: 'd',
+        describe: 'Path destination of files during moving or copying.\n'
+    })
+    .option('action', {
+        alias: 'a',
+        describe: 'Action to be performed on files. [copy, move or delete]\n cdm -a copy\n'
+    })
+    .demandOption(['filter'], 'You must provide a filter option.')
+    .demandOption(['action'], 'You must provide an action.')
+
+    .help()
+    .argv
+const { exec } = require('child_process')
+const copy = require('../lib/copy')
+
+const path = () => {
+    if(!argv.path){
+        return process.cwd()
+    } else {
+        return argv.path
+    }
+}
+
+switch(argv.action){
+    case 'copy':
+        if(!argv.dest){ return console.log('You must provide a destination file.\n') }
+        copy.copy(argv.filter, path(), argv.dest)
+        break;
+
+    case 'move':
+        if(!argv.dest){ return console.log('You must provide a destination file.\n') }
+        move()
+        break;
+
+    case 'delete':
+        del()
+        break;
+
+    default:
+        break;
+}
+
+
+// console.log(argv.filter)
