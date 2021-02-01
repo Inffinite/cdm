7d9eeba5d7e85c45143d10f42a965f98e0e7a623
inffinite <louislaizer4@gmail.com>
2021-02-01 12:07:36 +0300

    Added chalk

--- bin/index.js
+++ bin/index.js
@@ -1,29 +1,33 @@
+const chalk = require('chalk')
+
 const argv = require('yargs')
     .option('filter', {
         alias: 'f',
-        describe: 'Filter for specific file or files.\n'
+        describe: `Filter for specific file or files.\n ${chalk.green('The likes of grep filters.\n')}`
     })
     .option('path', {
         alias: 'p',
-        describe: 'Path to files.\n Defaults to current directory\n'
+        describe: `Path to files.\n ${chalk.green('Defaults to current directory.')}\n`
     })
     .option('dest', {
         alias: 'd',
-        describe: 'Path destination of files during moving or copying.\n'
+        describe: `Path destination of files during moving or copying.\n ${chalk.green('Must be provided in [copy, move] actions.\n')}`
     })
     .option('action', {
         alias: 'a',
-        describe: 'Action to be performed on files. [copy, move or delete]\n cdm -a copy\n'
+        describe: `Action to be performed on files. [copy, move or delete]\n ${chalk.green('cdm -a copy')}\n`
     })
     .demandOption(['filter'], 'You must provide a filter option.')
     .demandOption(['action'], 'You must provide an action.')
 
     .help()
     .argv
+
 const { exec } = require('child_process')
 const copy = require('../lib/copy')
 const del = require('../lib/delete')
 const move = require('../lib/move')
+
 
 const path = () => {
     if(!argv.path){
